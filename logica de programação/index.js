b0ef815bf255bd251586b324453273eb21e17224
Pedro Mendes <106615792+pedroMendes182@users.noreply.github.com>
2022-06-14 13:08:19 -0300

    Indrotução JS

---
+++ logica de programação/index.js
@@ -0,0 +1,75 @@
+// let primeiroNome   = "Pedro";
+// let sobreNome      = "Mendes";
+// let idade          = "25";
+
+// console.log(`Olá meu nome é ${primeiroNome}, e meu segundo nome é ${sobreNome}. E eu tenho ${idade} anos.`)
+
+// let preco1     = 15
+// let preco2     = 25
+// let precoTotal = preco1 + preco2
+
+// console.log(`Comprei uma xicara por ${preco1} e dois copos por ${preco2}. No total gastei ${precoTotal}`);
+
+// let contador = 0;
+
+// console.log(contador);
+// contador ++
+// console.log(contador);
+// contador ++
+// console.log(contador);
+// contador ++
+// console.log(contador);
+// contador ++
+// console.log(contador);
+// contador ++
+// console.log(contador);
+
+// let anoAtual      = 2022;
+// let anoNascimento = 1996;
+// let idadeAtual    = anoAtual - anoNascimento;
+
+// console.log(`No ano ${anoAtual} eu vou fazer ${idadeAtual} anos de idades. `)
+
+// let alturaRetangulo    = 5;
+// let larguraRetangulo   = 7;
+// let areaRetangulo      = alturaRetangulo * larguraRetangulo
+
+// console.log(`Um retangulo largura de ${larguraRetangulo}cm e altura de ${alturaRetangulo}cm possui ${areaRetangulo}cm² de area total.`);
+
+// let diaAtual         = 14;
+// let mesAtual         = 06;
+// let anoAtual         = 2022;
+// let dataFormanda     = (`${diaAtual}/${mesAtual}/${anoAtual}`)
+
+// console.log (`A data de hoje é ${dataFormanda}`)
+
+// let valorA = 7
+// let valorB = 3
+// let valorC = ""
+
+// valorC = valorA
+// valorA = valorB
+// valorB = valorC
+
+// console.log(`Antes a variavel 'valorA' possuia o valor ${valorB}, agora ela possui ${valorA}`)
+// console.log(`Antes a variavel 'valorB' possuia o valor ${valorA}, agora ela possui ${valorB}`)
+
+// let meuNome = "Jose Pedro Pires Mendes"
+// console.log(meuNome.length)
+
+// console.log(`Meu nome possui ${meuNome.length} caracteres de comprimento`)
+
+// let preco        = 15
+// let precoFormado = "R$15,00"
+
+
+// console.log(`O valor ${preco} formatado ficaria assim ${precoFormado}`)
+
+let cumprimento          = "Bom Dia"
+let nome                = "Pedro"
+let sobreNome           = "Mendes"
+
+const newLocal = `${cumprimento}  ${nome}  ${sobreNome}`
+let mensagemCumprimento = newLocal 
+
+console.log(mensagemCumprimento)
